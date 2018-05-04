12823c4c59a0c2634601e6bfa8e275169bd30a0e
maspen <mattaspen@gmail.com>
2018-05-04 15:58:52 -0700

    implemented code in app to satisfy 'fetching' tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,11 @@
 var app = {
-  init: function() {},
+  init: function() {
+    this.server = 'http://parse.sfm8.hackreactor.com/';
+  },
   send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -15,6 +17,25 @@ var app = {
         console.error('chatterbox: Failed to send message', message);
       }
     });
-
-  }
+  },
+  fetch: function() {
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      //data: JSON.stringify(message),
+      contentType: 'application/json',
+      data: {
+        format: 'json'
+      },
+      success: function (data) {
+        console.log('chatterbox: Message sent received from GET', data);
+        return data;
+      },
+      error: function () {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send GET');
+        return null;
+      }
+    });
+  },
 };
