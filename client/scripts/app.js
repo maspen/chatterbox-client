c210787a3f8b2bac6d4215846fe35014b4288c74
maspen <mattaspen@gmail.com>
2018-05-04 15:20:37 -0700

    added code to app.js to pass tests in 'init' & sending' section

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,20 @@
-// YOUR CODE HERE:
+var app = {
+  init: function() {},
+  send: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (message) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (message) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', message);
+      }
+    });
+
+  }
+};
